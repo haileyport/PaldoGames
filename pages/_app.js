ecf71f586bdbed0ea4f555cf520fa9d14db6d0c0
hongdahee <107875003+hongdahee@users.noreply.github.com>
2022-09-08 10:27:07 +0900

    ✒️Feat: 로그인 기능 테스트 (#14)
    * ✒️Feat: 로그인 기능 테스트
    
    * 🛠Fix SessionProvider 추가
    
    * 🎆Style: data를 data:session으로 변경
    
    🔬Chore: nextAuth adapter 설치

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
-import '../styles/globals.css';
+import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <RecoilRoot>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
       </RecoilRoot>
-    </>
-  );
+    </SessionProvider>
+  )
 }
 
 export default MyApp;
