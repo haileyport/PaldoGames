5be864d9456adf07f36e0b17e60a2e3645366a0f
hongdahee <heewhy3513@gmail.com>
2022-09-12 19:49:21 +0900

    ✒️Feat: 구구단 게임 기능 구현(타이머, 생명) ✍️Design: 센터 정렬, 배경색 추가

---
+++ components/Games/TimesTables/TimesTablesGame.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledTimesTables = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #1e293b;
+`;
+
+export { StyledTimesTables };
---
+++ pages/timestables.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import TimesTablesGame from '../components/Games/TimesTables/TimesTablesGame';
+import { StyledTimesTables } from '../components/Games/TimesTables/TimesTablesGame.style';
+
+
+const TimesTables = () => {
+
+  return(
+    <>
+    <Head>
+        <title>구구단 게임</title>
+    </Head>
+    <StyledTimesTables>
+    <TimesTablesGame />
+    </StyledTimesTables>
+    </>
+)
+}
+export default TimesTables;
