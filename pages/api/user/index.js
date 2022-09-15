a498b504f4e0b281c79524a6cdb7594964c6c827
leezer94 <2kunhee94@gmail.com>
2022-09-15 18:23:33 +0900

    🚀Merge: API 변경본 머지

---
+++ pages/api/user/index.js
@@ -0,0 +1,19 @@
+import prisma from "../../../libs/client";
+
+export default async (req, res) => {
+  if (req.method === "GET") {
+    const users = await prisma.user.findMany({});
+    res.json({ ok: true, users });
+  } else if (req.method === "PATCH") {
+    try {
+      const { userId, aboutMe } = req.body;
+      const response = await prisma.user.update({
+        where: { id: userId },
+        data: { aboutMe: aboutMe },
+      });
+      res.json(response);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
