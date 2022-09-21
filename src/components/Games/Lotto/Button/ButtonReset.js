49381c5706f5791abb9bb2bc87e0b5b5b7c89e0b
Exist95 <jyeol0210@naver.com>
2022-09-21 20:05:27 +0900

    🧺Remove: pull로 만들어진 데이터 삭제
    🧺Remove: pull로 만들어진 데이터 삭제

--- src/components/Games/Lotto/Button/ButtonReset.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ResetButton } from "./ButtonReset.style";
-
-const ButtonReset = ({ reset }) => {
-  return <ResetButton onClick={reset}>RESET</ResetButton>;
-};
-
-export default ButtonReset;
