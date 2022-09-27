e7269d6f64ec4fe5351ced8bc74c1e68854625d5
leezer94 <2kunhee94@gmail.com>
2022-09-27 15:44:41 +0900

    🛠Fix버그 수정 구구단 5문제 500 포인트 수정 글 삭제시 100 포인트 미만은 차감 안되게 설정 100 포인트 미만 보유시 게임존 접근 불가 설정

--- src/pages/games/index.js
+++ src/pages/games/index.js
@@ -1,8 +1,36 @@
+import axios from "axios";
 import { getSession } from "next-auth/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useRecoilValue } from "recoil";
 import { GamesMain } from "../../components";
+import { currentUserState } from "../../states";
 
 const GamesPage = () => {
+  const { user } = useRecoilValue(currentUserState);
+  const router = useRouter();
+
+  const fetchTotalPoint = async () => {
+    const { data } = await axios.get(`/api/game/${user.id}`);
+    let point;
+
+    if (data.response) {
+      point = data.response.totalPoint;
+
+      return point;
+    }
+  };
+
+  useEffect(() => {
+    fetchTotalPoint().then((totalPoint) => {
+      if (totalPoint < 100 && totalPoint !== undefined) {
+        router.push("/");
+        alert("현재 보유 포인트가 100 포인트보다 작습니다.");
+      }
+    });
+  }, [fetchTotalPoint, router]);
+
   return (
     <>
       <Head>
