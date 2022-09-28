02ebcda2cc5c545fa49ef005ec5fe8a9f0972196
hongdahee <107875003+hongdahee@users.noreply.github.com>
2022-09-28 22:21:11 +0900

    🚀Merge: dahee => develop (#75)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
+
 import { getSession } from "next-auth/react";
 import Head from "next/head";
 import { Main } from "../components";
@@ -7,8 +11,8 @@ export const Home = () => {
     <>
       <Head>
         <title>팔도게임즈!</title>
-        <link rel='icon' href='/favicon.ico' />
-        <meta name='description' content='오늘도 즐겜' />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="오늘도 즐겜" />
       </Head>
       <Main />
     </>
